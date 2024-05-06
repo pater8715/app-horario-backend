9b68777da15b872f2f452204523dff97b2e65882
pater8715 <pater8715@gmail.com>
2024-05-06 18:56:02 -0500

    Se creo el backend del proyecto app-horario

---
+++ horario-app-backend/src/main/java/com/uniremington/edu/co/horario/app/horarioappbackend/HorarioAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uniremington.edu.co.horario.app.horarioappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorarioAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorarioAppBackendApplication.class, args);
+	}
+
+}
---
+++ horario-app-backend/src/test/java/com/uniremington/edu/co/horario/app/horarioappbackend/HorarioAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniremington.edu.co.horario.app.horarioappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorarioAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
