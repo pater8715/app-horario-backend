c9d006759eb7b46e5a0b00c23f5cb6611ffce971
pater8715 <pater8715@gmail.com>
2024-05-06 19:11:14 -0500

    se actualizan los directorios del proyecto

--- src/main/java/com/uniremington/edu/co/horario/app/HorarioAppBackendApplication.java
+++ src/main/java/com/uniremington/edu/co/horario/app/HorarioAppBackendApplication.java
@@ -1,4 +1,4 @@
-package com.uniremington.edu.co.horario.app.horarioappbackend;
+package com.uniremington.edu.co.horario.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
